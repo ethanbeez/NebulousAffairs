3fc40b2295f6de78d901e31fe31c7f279c5a69ae
Orionicles <94778171+Orionicles@users.noreply.github.com>
2023-12-17 08:11:09 -0700

    Trade Dialogue input complete
    Dialogue is out of order, but I don't know how to fix that...

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -13,6 +13,7 @@ public class DialogueController : MonoBehaviour
     private List<Notification> currentNotifs;
     UIController uiController;
     Notification currentNotif;
+    TradeAction currentTrade;
 
     public void OnEnable()
     {
@@ -26,6 +27,7 @@ public class DialogueController : MonoBehaviour
         this.currentLeader = currentLeader;
         ReadNotifs();
         GenerateButtons();
+        uiController.AddToConverse("Greetings!");
     }
 
     private void ReadNotifs()
@@ -84,12 +86,15 @@ public class DialogueController : MonoBehaviour
                 return;
             }
         }
-        DisplayMessage(notifs[0]);
+        DisplayMessage(currentNotifs[0]);
     }
 
    private void DisplayTrade(Notification notif)
     {
+        Debug.Log("DisplayingTrade");
+        var tradetext = OfferTradeText(playerLeader.OutstandingTrades[currentLeader.Name]);
         uiController.AddToConverse(notif.NotificationMessage);
+        uiController.AddToConverse("I'll offer you " + tradetext[0].Item2 + " " + tradetext[0].Item1 + "in exchange for " + tradetext[1].Item2 + " of your " + tradetext[1].Item1);
         var button = Instantiate(DialogueButton);
         currentNotifs.Remove(notif);
         currentNotif = notif;
@@ -103,6 +108,42 @@ public class DialogueController : MonoBehaviour
 
     }
 
+    private List<(string, int)> OfferTradeText(TradeAction tradeAction)
+    {
+        var output = new List<(string, int)>();
+        if(tradeAction.OfferedAffluence > 0)
+        {
+            output.Add(("Wealth", tradeAction.OfferedAffluence));
+        }
+        if (tradeAction.OfferedIntellect > 0)
+        {
+            output.Add(("Influence", tradeAction.OfferedIntellect));
+        }
+        if (tradeAction.OfferedPolitics > 0)
+        {
+            output.Add(("Politics", tradeAction.OfferedPolitics));
+        }
+
+        if (tradeAction.RequestedAffluence > 0)
+        {
+            output.Add(("Wealth", tradeAction.RequestedAffluence));
+        }
+        if (tradeAction.RequestedIntellect > 0)
+        {
+            output.Add(("Influence", tradeAction.RequestedIntellect));
+        }
+        if (tradeAction.RequestedPolitics > 0)
+        {
+            output.Add(("Politics", tradeAction.RequestedPolitics));
+        }
+
+        if(output.Count > 2)
+        {
+            Debug.LogError("Too Many Trade Variables: " + output.Count);
+        }
+        return output;
+    }
+
     private void DisplayMessage(Notification notif)
     {
         uiController.AddToConverse(notif.NotificationMessage);
@@ -111,12 +152,15 @@ public class DialogueController : MonoBehaviour
         currentNotif = notif;
         button.GetComponent<Button>().onClick.AddListener(MessageConfirm);
         button.GetComponentInChildren<TextMeshProUGUI>().text = "OK";
+        
         uiController.AddConverseButton(button.GetComponent<Button>());
     }
 
     private void TradeConfirm()
     {
+        Debug.Log("Trade Confirmed");
         playerLeader.ProcessOutstandingTrade(currentLeader.Name, true );
+        Debug.Log("Trade Processed");
         uiController.ClearConverseButtons();
         currentNotifs.Remove(currentNotif);
         GenerateButtons();
